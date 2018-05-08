0cb3fd42735271bb671dfd6de6a774f5a64aac6d
AndrianCode <anpojda@gmail.com>
2018-05-08 15:52:52 +0300

    front

--- app.py
+++ app.py
@@ -1,12 +1,20 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 
 @app.route('/')
-def hello_world():
-    return 'Hello World!'
+def template_test():
+    return render_template('home.html',
+                           themes=[{"theme": "machine learning"}, {"theme": "kek"}, {"theme": "lol"}])
+
+
+@app.route('/<code>')
+def template_test2(code):
+    authors = [{"author": "Yarik", "messagesAmount": 1}, {"author": "Max", "messagesAmount": 6},
+               {"author": "Andrian", "messagesAmount": 4}, {"author": "Vadem", "messagesAmount": 8}]
+    return render_template('forum.html', authors=authors)
 
 
 if __name__ == '__main__':
-    app.run()
+    app.run(port=3000)
