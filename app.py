55f3a7ca7c0c7af5c18c9f3cb0642149adce7904
AndrianCode <anpojda@gmail.com>
2018-05-08 17:57:05 +0300

    front interaction with db

--- app.py
+++ app.py
@@ -1,23 +1,28 @@
 from flask import Flask, render_template
+from database.mongoDB import Database
 
+db = Database()
 app = Flask(__name__)
 
 
 @app.route('/')
 def home():
-    return render_template('home.html',
-                           topics=[{"name": "machine learning", "_id": "asd"},
-                                   {"name": "kek", "_id": "123"}, {"name": "lol", "_id": "qwerty"}])
+    return render_template('home.html', topics=db.get_topics())
 
 
-@app.route('/<id>')
-def forum(id):
-    author_mess_amount = {"Yarik": 1, "Max": 6, "Andrian": 5, "Vadem": 8}
+@app.route('/topics/<topic_id>')
+def topic(topic_id):
+    author_mess_amount = db.get_messages_counter_by_topic(topic_id)
+    max_amount = get_max_value(author_mess_amount)
+    return render_template('topic.html', author_mess_amount=author_mess_amount, max_amount=max_amount)
+
+
+def get_max_value(dictionary: dict):
     max_amount = 0
-    for value in author_mess_amount.values():
+    for value in dictionary.values():
         if value > max_amount:
             max_amount = value
-    return render_template('forum.html', author_mess_amount=author_mess_amount, max_amount=max_amount)
+    return max_amount
 
 
 if __name__ == '__main__':
