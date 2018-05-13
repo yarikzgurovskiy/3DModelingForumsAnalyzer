762486ec9be46cca0280cff0ed409abc489d417f
AndrianCode <anpojda@gmail.com>
2018-05-13 20:40:58 +0300

    implemented data updating feature

--- app.py
+++ app.py
@@ -1,26 +1,26 @@
 from flask import Flask, render_template, request
 from forum_grabber.forum_grabber.database.mongoDB import Database
-from forum_grabber.scraper import Scraper
+from subprocess import call
+import os
 
 db = Database()
 app = Flask(__name__)
-scraper = Scraper()
-scraper.scrap_topics()
-scraper.scrap_messages()
-scraper.run()
+os.chdir('forum_grabber')
+call(['scrapy', 'crawl', 'topics'])
+call(['scrapy', 'crawl', 'messages'])
 
 
 @app.route('/', methods=['GET', 'POST'])
 def home():
     if request.method == 'POST':
-        scraper.scrap_topics()
+        call(['scrapy', 'crawl', 'topics'])
     return render_template('home.html', topics=db.get_topics())
 
 
 @app.route('/topics/<topic_id>', methods=['GET', 'POST'])
 def topic(topic_id):
     if request.method == 'POST':
-        scraper.scrap_messages()
+        call(['scrapy', 'crawl', 'messages'])
     author_mess_amount = db.get_messages_counter_by_topic_id(topic_id)
     topic_url = db.get_topic_by_id(topic_id)['url']
     key_with_max_value = max(author_mess_amount.keys(), key=(lambda k: author_mess_amount[k]))
